2805e656d7c8aa46ad2df3af7f29703123b0e7dc
Jathin Pranav Singaraju <66273559+jp-singaraju@users.noreply.github.com>
2022-04-23 17:01:47 -0500

    resource page updates

--- src/Components/Resources.js
+++ src/Components/Resources.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Resources = () => {
   return (
-    <div>Resources</div>
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div class="max-w-md w-full space-y-8">
+        <div>
+          <h2 class="mt-8 text-center text-4xl font-extrabold text-gray-900">Find All Your Information Here.</h2>
+        </div>
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <label for="search-entry" class="sr-only">Enter Keywords</label>
+            <input id="search-entry" name="search" type="search" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address"/>
+          </div>
+          &nbsp;
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md rounded-t-md rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"/>
+          </div>
+        </div>
+        <div>
+          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-500 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mb-5">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -5,7 +5,7 @@ const Signup = () => {
     <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
   <div class="max-w-md w-full space-y-8">
     <div>
-      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Create Your Account</h2>
     </div>
     <form class="mt-8 space-y-6" action="#" method="POST">
       <div class="rounded-md shadow-sm -space-y-px">
