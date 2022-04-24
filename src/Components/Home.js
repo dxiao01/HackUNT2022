c2df6cf45c96293341c798814e7b8a29d82475e8
dch777 <d.chanana777@gmail.com>
2022-04-24 00:49:06 -0500

    Added login

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,83 +1,100 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { defineCustomElements } from 'shooting-stars/dist/loader';
-import star from "./star.png"
+import { defineCustomElements } from "shooting-stars/dist/loader";
 
 const Home = () => {
-    useEffect(() => {
-        AOS.init({
-          duration : 2000
-        });
-      }, []);
-      defineCustomElements(window);
-  return (
-    
-    <div class="h-full mb-4 scrollbar-hide">
-        <shooting-stars image={star} height="'10px'" width="10" min-speed="10" max-speed="20" num="100"></shooting-stars>
-        <div class="text-center text-7xl font-extrabold mt-10" data-aos="fade-up">
-            Spaceverse
-        </div>
-        <div class=" text-left text-3xl font-extrabold mt-48 ml-72" data-aos="fade-right">
-        Your Universe for All Things Space!
-        </div>
-        <div class="text-left text-3xl font-bold my-48 mx-60 float-left" data-aos="fade-left">
-        Spaceverse is a novel space-based education software. It brings all the resources from around the world together to one location in order help humanity understand recent space advancements, complex universal concepts, human exploration frontiers, and basically everything in the universe (pun intended). The goal of Spaceverse is to educate the community about the universe by consolidating information from news websites, allowing users to contribute to information by creating videos/websites, and gamifying the process of learning. 
-        </div>
-        <div class="max-w-6xl px-12 mx-auto text-center mb-10">
-            <div class="space-y-12 md:text-center" data-aos="fade-up">
-                <div class="max-w-3xl mb-10 space-y-5 sm:mx-auto sm:space-y-4">
-                    <h2 class="relative text-4xl font-extrabold tracking-tight sm:text-5xl mt-10">About Us</h2>
-                </div>
-            </div>
-    
-            <div class="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
-                <div class="w-full border border-gray-200 rounded-lg shadow-lg" data-aos="flip-down">
-    
-                    <div class="flex flex-col items-center justify-center p-10">
-                        <h2 class="text-lg font-medium">Daniel Xiao</h2>
-                        <p class="font-medium text-blue-500">Front-end designer </p>
-                        </div>
-    
-                </div>
-    
-                <div class="w-full border border-gray-200 rounded-lg shadow-lg" data-aos="flip-down">
-    
-                    <div class="flex flex-col items-center justify-center p-10">
-                        <h2 class="text-lg font-medium">Dhruv Chanana</h2>
-                        <p class="font-medium text-blue-500">Backend developer</p>
-                        </div>
-    
-    
-                </div>
-    
-                <div class="w-full border border-gray-200 rounded-lg shadow-lg" data-aos="flip-down">
-    
-                    <div class="flex flex-col items-center justify-center p-10">
-                        <h2 class="text-lg font-medium">Jathin Pranav Singaraju</h2>
-                        <p class="font-medium text-blue-500">Front-end designer</p>
-                        </div>
-    
-    
-                </div>
-    
-                <div class="w-full border border-gray-200 rounded-lg shadow-lg" data-aos="flip-down">
-    
-                    <div class="flex flex-col items-center justify-center p-10">
-                        <h2 class="text-lg font-medium">James Yang</h2>
-                        <p class="font-medium text-blue-500">Logistics</p>
-                        </div>
-    
-    
-                </div>
-    
-    
-            </div>
-    
-        </div>
-    </div>
-  )
+	useEffect(() => {
+		AOS.init({
+			duration: 2000,
+		});
+	}, []);
+	defineCustomElements(window);
+	return (
+		<div class="h-full mb-4 scrollbar-hide">
+			<shooting-stars
+				image="/star"
+				height="'10px'"
+				width="10"
+				min-speed="10"
+				max-speed="20"
+				num="100"
+			></shooting-stars>
+			<div class="text-center text-7xl font-extrabold mt-10" data-aos="fade-up">
+				Spaceverse
+			</div>
+			<div
+				class=" text-left text-3xl font-extrabold mt-48 ml-72"
+				data-aos="fade-right"
+			>
+				Your Universe for All Things Space!
+			</div>
+			<div
+				class="text-left text-3xl font-bold my-48 mx-60 float-left"
+				data-aos="fade-left"
+			>
+				Spaceverse is a novel space-based education software. It brings all the
+				resources from around the world together to one location in order help
+				humanity understand recent space advancements, complex universal
+				concepts, human exploration frontiers, and basically everything in the
+				universe (pun intended). The goal of Spaceverse is to educate the
+				community about the universe by consolidating information from news
+				websites, allowing users to contribute to information by creating
+				videos/websites, and gamifying the process of learning.
+			</div>
+			<div class="max-w-6xl px-12 mx-auto text-center mb-10">
+				<div class="space-y-12 md:text-center" data-aos="fade-up">
+					<div class="max-w-3xl mb-10 space-y-5 sm:mx-auto sm:space-y-4">
+						<h2 class="relative text-4xl font-extrabold tracking-tight sm:text-5xl mt-10">
+							About Us
+						</h2>
+					</div>
+				</div>
 
+				<div class="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
+					<div
+						class="w-full border border-gray-200 rounded-lg shadow-lg"
+						data-aos="flip-down"
+					>
+						<div class="flex flex-col items-center justify-center p-10">
+							<h2 class="text-lg font-medium">Daniel Xiao</h2>
+							<p class="font-medium text-blue-500">Front-end designer </p>
+						</div>
+					</div>
+
+					<div
+						class="w-full border border-gray-200 rounded-lg shadow-lg"
+						data-aos="flip-down"
+					>
+						<div class="flex flex-col items-center justify-center p-10">
+							<h2 class="text-lg font-medium">Dhruv Chanana</h2>
+							<p class="font-medium text-blue-500">Backend developer</p>
+						</div>
+					</div>
+
+					<div
+						class="w-full border border-gray-200 rounded-lg shadow-lg"
+						data-aos="flip-down"
+					>
+						<div class="flex flex-col items-center justify-center p-10">
+							<h2 class="text-lg font-medium">Jathin Pranav Singaraju</h2>
+							<p class="font-medium text-blue-500">Front-end designer</p>
+						</div>
+					</div>
+
+					<div
+						class="w-full border border-gray-200 rounded-lg shadow-lg"
+						data-aos="flip-down"
+					>
+						<div class="flex flex-col items-center justify-center p-10">
+							<h2 class="text-lg font-medium">James Yang</h2>
+							<p class="font-medium text-blue-500">Logistics</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,72 +1,89 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
-  return (
-  <div class="flex items-center justify-center">
-    <div class="bg-white w-3/4 mt-10 rounded-lg">
-      <div class="flex items-center justify-center pt-10 flex-col">
-        <img src="../../astronaut.jpg" class="rounded-full w-32"></img>
-        <h1 class="text-gray-800 font-extrabold text-3xl mt-5">Username</h1>
-        <h1 class="text-gray-500 text-sm">Total Stars: xxx</h1>
-        <h1 class="text-gray-500 text-sm">Post count: xxx</h1>
-        <h1 class="text-gray-500 text-sm p-4 text-center">
-          bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </h1>
-      </div>
-      <div class="flex items-center justify-center pt-10 flex-col">
-        <h1 class = "text-gray-800 font-extrabold text-3xl mt-5">Posts</h1>
-        <div class="w-full px-12 mx-auto text-center">
-            <div class="grid grid-cols-1">
-                <div class="w-full  border-gray-200 rounded-lg shadow-lg">
-                  <div class="flex justify-between p-10 max-h-80">
-                    <div class="flex flex-col items-left justify-left p-10">
-                        <h2 class="text-lg font-medium">How NASA Reinvented The Wheel</h2>
-                        <p class="font-medium text-grey-100">Article</p>
-                        <p class="text-black">Tags: Engineering, Materials</p>
-                    </div>  
-                    <div>  
-                        <img src = "../../wheel.jpeg" class ="h-60 w-60 object-scale-down"></img>
-                    </div>
-                  </div>
-                </div>
-      
-                   
-                <div class="w-full  border-gray-200 rounded-lg shadow-lg">
-                  <div class="flex justify-between p-10 max-h-80">
-                    <div class="flex flex-col items-left justify-left p-10">
-                        <h2 class="text-lg font-medium">MEGA Drive — the Tech That Promises Near Light-Speed Travel!</h2>
-                        <p class="font-medium text-grey-100">Article</p>
-                        <p class="text-black">Tags: Theoretical Physics, Interstellar Travel, Light-Speed</p>
-                    </div>  
-                    <div>  
-                        <img src = "../../mega engine.jpeg" class =" h-60 w-60 object-scale-down"></img>
-                    </div>
-                  </div>
-                </div>
-                
-                <div class="w-full  border-gray-200 rounded-lg shadow-lg">
-                  <div class="flex justify-between p-10 max-h-80">
-                    <div class="flex flex-col items-left justify-left p-10">
-                        <h2 class="text-lg font-medium">NASA’s Perseverance Rover arrives at delta for new science campaign</h2>
-                        <p class="font-medium text-grey-100">News</p>
-                        <p class="text-black">Tags: Mars, Robotic Exploration, Mapping</p>
-                    </div>  
-                    <div>  
-                        <img src = "../../mars.png" class =" h-60 w-60 object-scale-down"></img>
-                    </div>
-                  </div>
-                </div>
-    
-            </div>
-    
-        </div>
-      </div>
-    </div>
+	return (
+		<div class="flex items-center justify-center">
+			<div class="bg-white w-3/4 mt-10 rounded-lg">
+				<div class="flex items-center justify-center pt-10 flex-col">
+					<img src="/astronaut.jpg" class="rounded-full w-32"></img>
+					<h1 class="text-gray-800 font-extrabold text-3xl mt-5">Username</h1>
+					<h1 class="text-gray-500 text-sm">Total Stars: xxx</h1>
+					<h1 class="text-gray-500 text-sm">Post count: xxx</h1>
+					<h1 class="text-gray-500 text-sm p-4 text-center">
+						bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</h1>
+				</div>
+				<div class="flex items-center justify-center pt-10 flex-col">
+					<h1 class="text-gray-800 font-extrabold text-3xl mt-5">Posts</h1>
+					<div class="w-full px-12 mx-auto text-center">
+						<div class="grid grid-cols-1">
+							<div class="w-full  border-gray-200 rounded-lg shadow-lg">
+								<div class="flex justify-between p-10 max-h-80">
+									<div class="flex flex-col items-left justify-left p-10">
+										<h2 class="text-lg font-medium">
+											How NASA Reinvented The Wheel
+										</h2>
+										<p class="font-medium text-grey-100">Article</p>
+										<p class="text-black">Tags: Engineering, Materials</p>
+									</div>
+									<div>
+										<img
+											src="/wheel.jpeg"
+											class="h-60 w-60 object-scale-down"
+										></img>
+									</div>
+								</div>
+							</div>
 
-</div>
+							<div class="w-full  border-gray-200 rounded-lg shadow-lg">
+								<div class="flex justify-between p-10 max-h-80">
+									<div class="flex flex-col items-left justify-left p-10">
+										<h2 class="text-lg font-medium">
+											MEGA Drive — the Tech That Promises Near Light-Speed
+											Travel!
+										</h2>
+										<p class="font-medium text-grey-100">Article</p>
+										<p class="text-black">
+											Tags: Theoretical Physics, Interstellar Travel,
+											Light-Speed
+										</p>
+									</div>
+									<div>
+										<img
+											src="/megaengine.jpeg"
+											class=" h-60 w-60 object-scale-down"
+										></img>
+									</div>
+								</div>
+							</div>
 
+							<div class="w-full  border-gray-200 rounded-lg shadow-lg">
+								<div class="flex justify-between p-10 max-h-80">
+									<div class="flex flex-col items-left justify-left p-10">
+										<h2 class="text-lg font-medium">
+											NASA’s Perseverance Rover arrives at delta for new science
+											campaign
+										</h2>
+										<p class="font-medium text-grey-100">News</p>
+										<p class="text-black">
+											Tags: Mars, Robotic Exploration, Mapping
+										</p>
+									</div>
+									<div>
+										<img
+											src="/mars.png"
+											class=" h-60 w-60 object-scale-down"
+										></img>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-  )
-}
-
-export default Profile
+export default Profile;
